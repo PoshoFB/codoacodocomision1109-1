dba578e54b85bc1e52c473afb9ccb20e5f5bd875
PoshoFB <arqfernandezblanco@gmail.com>
2021-09-28 12:19:33 -0300

    Evento click, switch, Number, parseFloat, parseInt,Concatenar,Funciones

---
+++ Javascript/js/Clase3.js
@@ -0,0 +1,37 @@
+// let nombre = prompt("ingresa tu nombre");
+let miDiv = document.getElementById("principal");
+// miDiv.innerHTML="Hola "+nombre;
+
+let btn = document.getElementById("btn");
+btn.addEventListener('click',sumar);
+
+function sumar(){
+    // let num = parseInt(prompt("ingresa tu primer numero!"));
+    // let num2 = parseInt(prompt("ingresa tu segundo numero!"));
+    let num = parseFloat(prompt("ingresa tu primer numero!"));
+    let num2 = parseFloat(prompt("ingresa tu segundo numero!"));
+    let op = prompt("Escribi la operacion a realizar \n + para sumar\n - para restar\n * para multiplicar \n / para dividir")
+    let resultado=0;
+    switch (op) {
+        case "+":
+            resultado = num + num2;
+            miDiv.innerHTML +="<p> El resultado de la suma de "+num +" y "+num2+ " es: " + resultado + "</p>";
+            break;
+        case "-":
+            resultado = num - num2;
+            miDiv.innerHTML +="<p> El resultado de la resta de "+num +" y "+num2+ " es: " + resultado + "</p>";
+            break;
+        case "/":
+            resultado = num / num2;
+            miDiv.innerHTML +="<p> El resultado de la División de "+num +" y "+num2+ " es: " + resultado + "</p>";
+            break;
+        case "*":
+                resultado = num * num2;
+                miDiv.innerHTML +="<p> El resultado de la Multiplicación de "+num +" y "+num2+ " es: " + resultado + "</p>";
+                break;
+        default:
+
+            break;
+    }
+
+};
